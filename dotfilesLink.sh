f69680f928cb570fd226abfe70dd1bd6fd25a701
assout <assout@users.noreply.github.com>
2014-04-12 15:58:04 +0900

    adding

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,4 +2,4 @@
 ln -s ~/development/dotfiles/.vimrc ~/.vimrc
 ln -s ~/development/dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/development/dotfiles/.bash_profile ~/.bash_profile
-
+ln -s ~/development/dotfiles/.bashrc ~/.bashrc

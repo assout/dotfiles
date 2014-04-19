925dc36f8c228d3e88d1156d4cded76596d6e148
assout <assout@users.noreply.github.com>
2014-04-19 11:06:34 +0900

    update.

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -s ~/development/dotfiles/.vimrc ~/.vimrc
 ln -s ~/development/dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/development/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/development/dotfiles/.bashrc ~/.bashrc
+echo test

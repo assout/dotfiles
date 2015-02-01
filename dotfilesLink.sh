dd5351ba71f90c839f52e2223c6f58da55378b95
assout <assout@users.noreply.github.com>
2015-02-01 21:30:05 +0900

    test

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,6 +2,6 @@
 ln -s ~/development/dotfiles/_vimrc ~/.vimrc
 ln -s ~/development/dotfiles/_gvimrc ~/.gvimrc
 ln -s ~/development/dotfiles/_vrapperrc ~/.vrapperrc
-ln -s ~/development/dotfiles/_my_bashrc ~/.bashrc
+ln -sb ~/development/dotfiles/_my_bashrc ~/.bashrc
 ln -s ~/development/dotfiles/.Xmodmap ~/.Xmodmap
 

5c2a190d527ea86dc331b22206284749cc15c23c
assout <assout@users.noreply.github.com>
2015-02-17 01:00:37 +0900

    add .gitconfig

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ln -si ~/development/dotfiles/_vimrc ~/.vimrc
-ln -s ~/development/dotfiles/_gvimrc ~/.gvimrc
-ln -s ~/development/dotfiles/_vrapperrc ~/.vrapperrc
+ln -si ~/development/dotfiles/_gvimrc ~/.gvimrc
+ln -si ~/development/dotfiles/_vrapperrc ~/.vrapperrc
 ln -sb ~/development/dotfiles/_my_bashrc ~/.bashrc
-ln -s ~/development/dotfiles/.Xmodmap ~/.Xmodmap
+ln -si ~/development/dotfiles/.Xmodmap ~/.Xmodmap
+ln -si ~/development/dotfiles/.gitconfig ~/.gitconfig
 

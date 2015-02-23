62e77c5644f23bc30738d4b196739f65a6c74132
assout <assout@users.noreply.github.com>
2015-02-23 22:30:44 +0900

    add gitignore settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,4 +5,5 @@ ln -si ~/development/dotfiles/_vrapperrc ~/.vrapperrc
 ln -sb ~/development/dotfiles/_my_bashrc ~/.bashrc
 ln -si ~/development/dotfiles/.Xmodmap ~/.Xmodmap
 ln -si ~/development/dotfiles/.gitconfig ~/.gitconfig
+ln -si ~/development/dotfiles/.gitignore ~/.gitignore
 

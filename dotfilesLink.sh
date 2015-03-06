6311053dc1e8611380dc78634102699eab92d89b
assout <assout@users.noreply.github.com>
2015-03-06 23:05:02 +0900

    remove xmodmap settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,7 +2,6 @@
 ln -si ~/development/dotfiles/_vimrc ~/.vimrc
 ln -si ~/development/dotfiles/_gvimrc ~/.gvimrc
 ln -sb ~/development/dotfiles/.bashrc ~/.bashrc
-ln -si ~/development/dotfiles/.Xmodmap ~/.Xmodmap
 ln -si ~/development/dotfiles/.gitconfig ~/.gitconfig
 ln -si ~/development/dotfiles/.gitignore ~/.gitignore
 ln -si ~/development/dotfiles/eclipse/_vrapperrc ~/.vrapperrc

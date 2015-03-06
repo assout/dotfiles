96c7d280714b2f11ffe0938dadb26612293fe1f4
assout <assout@users.noreply.github.com>
2015-03-06 23:03:50 +0900

    change bashrc file name

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ln -si ~/development/dotfiles/_vimrc ~/.vimrc
 ln -si ~/development/dotfiles/_gvimrc ~/.gvimrc
-ln -sb ~/development/dotfiles/_my_bashrc ~/.bashrc
+ln -sb ~/development/dotfiles/_bashrc ~/.bashrc
 ln -si ~/development/dotfiles/.Xmodmap ~/.Xmodmap
 ln -si ~/development/dotfiles/.gitconfig ~/.gitconfig
 ln -si ~/development/dotfiles/.gitignore ~/.gitignore

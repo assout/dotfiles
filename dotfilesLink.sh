9d976d51cbea4e465563e5a4a8f0306d0e2fa058
assout <porinsan@gmail.com>
2015-03-06 22:35:01 +0900

    add and change chrome settings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 ln -si ~/development/dotfiles/_vimrc ~/.vimrc
 ln -si ~/development/dotfiles/_gvimrc ~/.gvimrc
-ln -si ~/development/dotfiles/_vrapperrc ~/.vrapperrc
 ln -sb ~/development/dotfiles/_my_bashrc ~/.bashrc
 ln -si ~/development/dotfiles/.Xmodmap ~/.Xmodmap
 ln -si ~/development/dotfiles/.gitconfig ~/.gitconfig
 ln -si ~/development/dotfiles/.gitignore ~/.gitignore
-
+ln -si ~/development/dotfiles/eclipse/_vrapperrc ~/.vrapperrc

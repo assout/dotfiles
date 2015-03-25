da84caf6f2cf239191dab29c8f4cc1cf5345ceb5
assout <porinsan@gmail.com>
2015-03-26 06:47:05 +0900

    add inputrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ln -si ~/development/dotfiles/_vimrc ~/.vimrc
-ln -si ~/development/dotfiles/_gvimrc ~/.gvimrc
-ln -sb ~/development/dotfiles/.bashrc ~/.bashrc
-ln -si ~/development/dotfiles/.gitconfig ~/.gitconfig
-ln -si ~/development/dotfiles/.gitignore ~/.gitignore
-ln -si ~/development/dotfiles/eclipse/_vrapperrc ~/.vrapperrc
+ln -si ~/Development/dotfiles/_vimrc ~/.vimrc
+ln -si ~/Development/dotfiles/_gvimrc ~/.gvimrc
+ln -si ~/Development/dotfiles/.bashrc ~/
+ln -si ~/Development/dotfiles/.inputrc ~/
+ln -si ~/Development/dotfiles/.gitconfig ~/
+ln -si ~/Development/dotfiles/.gitignore ~/
+ln -si ~/Development/dotfiles/eclipse/_vrapperrc ~/.vrapperrc

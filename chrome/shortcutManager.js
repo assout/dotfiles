78f64e875f4c0d838eee0122ff29131a06773422
assout <assout@users.noreply.github.com>
2015-05-30 22:47:07 +0900

    initial commit

---
+++ chrome/shortcutManager.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @ShortcutManager
+// @name disable
+// @namespace fVnhlldvzfzZ
+// @key CTRL+J
+// @include *
+// ==/UserScript==
+// 何も書かないと、[CTRL+J] は無効化されます
+
+// ==UserScript==
+// @ShortcutManager
+// @name disable
+// @namespace fVnhlldvzfzZ
+// @key CTRL+K
+// @include *
+// ==/UserScript==
+// 何も書かないと、[CTRL+K] は無効化されます
+
+// ==UserScript==
+// @ShortcutManager
+// @name 左のタブを選択する
+// @namespace fVnhlldvzfzZ
+// @key CTRL+H
+// @include *
+// @execute SelectLeftTab([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name 1ページ下へ
+// @namespace fVnhlldvzfzZ
+// @key CTRL+F
+// @include *
+// @execute PageDown([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name 右のタブを選択する
+// @namespace fVnhlldvzfzZ
+// @key CTRL+L
+// @include *
+// @execute SelectRightTab([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name 左のタブを全て閉じる
+// @namespace fVnhlldvzfzZ
+// @key Ctrl+Shift+h
+// @include *
+// @execute CloseLeftTabs([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name 右のタブを全て閉じる
+// @namespace fVnhlldvzfzZ
+// @key Ctrl+Shift+l
+// @include *
+// @execute CloseRightTabs([])
+// ==/UserScript==
+
+// ==UserScript==
+// @ShortcutManager
+// @name 他のタブを全て閉じる
+// @namespace fVnhlldvzfzZ
+// @key Ctrl+Shift+o
+// @include *
+// @execute CloseOtherTabs([])
+// ==/UserScript==

1a4f652ec1e45b52da8019b3aea6de97c8658590
assout <assout@users.noreply.github.com>
2015-08-16 08:28:31 +0900

    change vim dotfiles structures

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ln -si ~/Development/dotfiles/_vimrc ~/.vimrc
-ln -si ~/Development/dotfiles/.vimrc.devlopment ~/Development/vim-plugins
-ln -si ~/Development/dotfiles/_gvimrc ~/.gvimrc
+ln -si ~/Development/dotfiles/vim/_vimrc ~/.vimrc
+ln -si ~/Development/dotfiles/vim/.vimrc.devlopment ~/Development/vim-plugins
+ln -si ~/Development/dotfiles/vim/_gvimrc ~/.gvimrc
 ln -si ~/Development/dotfiles/.bashrc ~/
 ln -si ~/Development/dotfiles/.inputrc ~/
 ln -si ~/Development/dotfiles/.gitconfig ~/

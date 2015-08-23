c505a339afca1476834da5a48ce383fbbd9c050d
assout <assout@users.noreply.github.com>
2015-08-23 23:25:39 +0900

    Changes to be committed: 	modified:   .bashrc 	modified:   .gitconfig 	new file:   .mdlrc 	modified:   dotfilesLink.sh 	modified:   vim/_vimrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+ln -si ~/Development/dotfiles/.bashrc ~/
+ln -si ~/Development/dotfiles/.inputrc ~/
+ln -si ~/Development/dotfiles/.mdlrc ~/
+ln -si ~/Development/dotfiles/.gitconfig ~/
+ln -si ~/Development/dotfiles/.gitignore ~/
 ln -si ~/Development/dotfiles/vim/_vimrc ~/.vimrc
 ln -si ~/Development/dotfiles/vim/.vimrc.devlopment ~/Development/vim-plugins
 ln -si ~/Development/dotfiles/vim/_gvimrc ~/.gvimrc
-ln -si ~/Development/dotfiles/.bashrc ~/
-ln -si ~/Development/dotfiles/.inputrc ~/
-ln -si ~/Development/dotfiles/.gitconfig ~/
-ln -si ~/Development/dotfiles/.gitignore ~/
 ln -si ~/Development/dotfiles/eclipse/_vrapperrc ~/.vrapperrc
 ln -si ~/Development/dotfiles/eclipse/_vrapperrc.linux ~/.vrapperrc.local

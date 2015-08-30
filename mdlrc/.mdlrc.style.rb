ab7d9d3c5d30859dde77afcabab15b7ac2ada05a
assout <assout@users.noreply.github.com>
2015-08-30 21:10:53 +0900

    change mdlrc

---
+++ mdlrc/.mdlrc.style.rb
@@ -0,0 +1,12 @@
+all
+# TODO MD007は無効化したくないがハードタブだとエラーになってしまうっぽい
+exclude_rule 'MD007'
+exclude_rule 'MD010'
+exclude_rule 'MD013'
+exclude_rule 'MD024'
+exclude_rule 'MD029'
+exclude_rule 'MD033'
+exclude_rule 'MD040'
+
+rule 'MD009', :br_spaces => 2
+rule 'MD026', :punctuation => ",;:"

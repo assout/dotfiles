d31ca1440fca139ee684e0bc5f873140d4bb76c4
assout <assout@users.noreply.github.com>
2015-08-31 03:20:51 +0900

    change setup.sh name

---
+++ setup.bat
@@ -0,0 +1,8 @@
+mklink %HOME%"\_vimrc" "D:\admin\Development\dotfiles\vim\_vimrc"
+mklink %HOME%"\_gvimrc" "D:\admin\Development\dotfiles\vim\_gvimrc"
+mklink %HOME%"\.bashrc" "D:\admin\Development\dotfiles\.bashrc"
+mklink %HOME%"\.bashrc" "D:\admin\Development\dotfiles\.bashrc"
+mklink %HOME%"\.gitconfig" "D:\admin\Development\dotfiles\.gitconfig"
+mklink %HOME%"\.gitignore" "D:\admin\Development\dotfiles\.gitignore"
+mklink "D:\admin\_vrapperrc" "D:\admin\Development\dotfiles\eclipse\_vrapperrc"
+mklink "D:\admin\.vrapperrc.local" "D:\admin\Development\dotfiles\eclipse\_vrapperrc.win"

b50bff2bd434a1526ed45fae2e00616e76fdba66
assout <assout@users.noreply.github.com>
2015-09-01 00:59:13 +0900

    Changes to be committed: 	modified:   install.sh 	modified:   vim/_gvimrc

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ ln -sf ~/Development/dotfiles/.tmux.conf ~/
 ln -sf ~/Development/dotfiles/.inputrc ~/
 ln -sf ~/Development/dotfiles/.gitconfig ~/
 ln -sf ~/Development/dotfiles/.gitignore ~/
-ln -sf ~/Development/dotfiles/mdlrc/.mdlrc ~/
-ln -sf ~/Development/dotfiles/mdlrc/.mdlrc.style.rb ~/
+ln -sf ~/Development/dotfiles/markdown/.mdlrc ~/
+ln -sf ~/Development/dotfiles/markdown/.mdlrc.style.rb ~/
 ln -sf ~/Development/dotfiles/vim/_vimrc ~/.vimrc
 ln -sf ~/Development/dotfiles/vim/.vimrc.devlopment ~/Development/vim-plugins
 ln -sf ~/Development/dotfiles/vim/_gvimrc ~/.gvimrc

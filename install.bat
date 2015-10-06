9176fde7994df69193beb7348c2ec80a644b3837
assout <assout@users.noreply.github.com>
2015-10-06 14:11:05 +0900

    update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
+mklink /J "D:\admin\dotfiles" "D:\admin\Development\dotfiles"
 mklink %HOME%"\_vimrc" "D:\admin\Development\dotfiles\vim\_vimrc"
 mklink %HOME%"\_gvimrc" "D:\admin\Development\dotfiles\vim\_gvimrc"
 mklink %HOME%"\.bashrc" "D:\admin\Development\dotfiles\.bashrc"
@@ -6,3 +7,6 @@ mklink %HOME%"\.gitconfig" "D:\admin\Development\dotfiles\.gitconfig"
 mklink %HOME%"\.gitignore" "D:\admin\Development\dotfiles\.gitignore"
 mklink "D:\admin\_vrapperrc" "D:\admin\Development\dotfiles\eclipse\_vrapperrc"
 mklink "D:\admin\.vrapperrc.local" "D:\admin\Development\dotfiles\eclipse\_vrapperrc.win"
+
+git config user.name = "assout"
+git config user.email = assout@users.noreply.github.com

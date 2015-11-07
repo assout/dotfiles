e8a025703098c278a7d3243141c55a3dcea01f6e
assout <assout@users.noreply.github.com>
2015-11-07 19:58:59 +0900

    fix directory structure

--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ ln -sf ~/Development/dotfiles/.gitconfig.linux ~/.gitconfig.environment
 ln -sf ~/Development/dotfiles/.gitignore ~/
 ln -sf ~/Development/dotfiles/.inputrc ~/
 ln -sf ~/Development/dotfiles/.tmux.conf ~/
-ln -sf ~/Development/dotfiles/.textlintrc ~/
 ln -sf ~/Development/dotfiles/eclipse/_vrapperrc ~/.vrapperrc
 ln -sf ~/Development/dotfiles/eclipse/_vrapperrc.linux ~/.vrapperrc.environment
-ln -sf ~/Development/dotfiles/markdown/.mdlrc ~/
-ln -sf ~/Development/dotfiles/markdown/.mdlrc.style.rb ~/
+ln -sf ~/Development/dotfiles/lint/.mdlrc ~/
+ln -sf ~/Development/dotfiles/lint/.mdlrc.style.rb ~/
+ln -sf ~/Development/dotfiles/lint/.textlintrc ~/
 ln -sf ~/Development/dotfiles/vim/.gvimrc ~/
 ln -sf ~/Development/dotfiles/vim/.vimrc ~/
 ln -sf ~/Development/dotfiles/vim/.vimrc.devlopment ~/Development/vim-plugins

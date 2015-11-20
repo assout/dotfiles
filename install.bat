38d76c4878a2c17b1505650f750a088c97187df5
assout <assout@users.noreply.github.com>
2015-11-20 16:45:04 +0900

    update

--- install.bat
+++ install.bat
@@ -11,8 +11,11 @@ mklink "%HOME%\.minttyrc" "%HOME%\Development\dotfiles\.minttyrc"
 mklink "%HOME%\.tmux.conf" "%HOME%\Development\dotfiles\.tmux.conf"
 mklink "%HOME%\_gvimrc" "%HOME%\Development\dotfiles\vim\.gvimrc"
 mklink "%HOME%\_vimrc" "%HOME%\Development\dotfiles\vim\.vimrc"
+REM TODO Eclipse Marsは%USERPROFILE%を見る。Keplerは%HOME%を見る。
 mklink "%HOME%\.vrapperrc.environment" "%HOME%\Development\dotfiles\eclipse\_vrapperrc.win"
+mklink "%USERPROFILE%\.vrapperrc.environment" "%HOME%\Development\dotfiles\eclipse\_vrapperrc.win"
 mklink "%HOME%\_vrapperrc" "%HOME%\Development\dotfiles\eclipse\_vrapperrc"
+mklink "%USERPROFILE%\_vrapperrc" "%HOME%\Development\dotfiles\eclipse\_vrapperrc"
 
 git config user.name "assout"
 git config user.email "assout@users.noreply.github.com"

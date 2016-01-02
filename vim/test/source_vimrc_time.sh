b3dd3ca3f6ea7044305da14ded55c3efd727ebd5
assout <assout@users.noreply.github.com>
2016-01-03 00:54:00 +0900

    Fix performance shell

--- vim/test/source_vimrc_time.sh
+++ vim/test/source_vimrc_time.sh
@@ -29,7 +29,7 @@ echo "${temp_proc}"
 
 echo "1, 2, 3, 4, 5, 6, 7, 8, 9, 10" > "${result_file}"
 paste -d ',' -s "${temp_time}" >> "${result_file}"
-# paste -d ',' -s "${temp_proc}" "${temp_time}" > "${result_file}"
 
+# paste -d ',' -s "${temp_proc}" "${temp_time}" > "${result_file}"
 # echo "time, proc" > "${result_file}"
 # paste -d ',' "${temp_time}" "${temp_proc}" >> "${result_file}"

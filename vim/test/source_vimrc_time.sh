8f3dd950dda0ebcb671b2861e3028e2f7993aec1
assout <assout@users.noreply.github.com>
2016-01-02 21:37:42 +0900

    Add souce vimrc time shell

---
+++ vim/test/source_vimrc_time.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+readonly target_dir="${WORKSPACE:-.}/target"
+mkdir -p "${target_dir}"
+result_file=${target_dir}/result_source.csv
+
+readonly MYVIMRC="${WORKSPACE:-~/dotfiles}/vim/.vimrc"
+
+temp_raw=$(mktemp)
+vim -u "${MYVIMRC}" -e -c "BenchVimrc ${MYVIMRC} ${temp_raw} | qa!"
+
+temp_sorted=$(mktemp)
+sed -e '/^        .*/d' "${temp_raw}" | sort -r | head -20 > "${temp_sorted}"
+
+temp_time=$(mktemp)
+temp_proc=$(mktemp)
+cut "${temp_sorted}" -c 3-11 > "${temp_time}"
+cut "${temp_sorted}" -c 12- | sed -e 's/,/、/g' > "${temp_proc}"
+
+echo "${temp_time}"
+
+echo "${temp_proc}"
+
+paste -d ',' -s "${temp_proc}" "${temp_time}" > "${result_file}"
+
+
+# echo "time, proc" > "${result_file}"
+# paste -d ',' "${temp_time}" "${temp_proc}" >> "${result_file}"

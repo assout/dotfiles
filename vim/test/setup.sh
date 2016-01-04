fd9cbe0645e5800cd2f6fe1ca2f7537093d3c3eb
assout <assout@users.noreply.github.com>
2016-01-05 03:51:47 +0900

    Change NeoBundle to Vim-Plug

--- vim/test/setup.sh
+++ vim/test/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-bundle_dir=${WORKSPACE}/target/bundle
-mkdir -p "${bundle_dir}"
-git clone https://github.com/Shougo/neobundle.vim "${bundle_dir}/neobundle.vim"
+vim_dir=${WORKSPACE}/.vim
 
-vim -N -u "${WORKSPACE}"/vim/.vimrc \
--c "try | NeoBundleUpdate! $* | finally | qall! | endtry" \
--U NONE -i NONE -V1 -e -s || :
+curl -fLo ${vim_dir}/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# vim -N -u "${WORKSPACE}"/vim/.vimrc -c "try | set rtp+=${vim_dir} \| PlugUpdate! $* | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
+vim -V -N -u "${WORKSPACE}"/vim/.vimrc -c "PlugUpdate" +qa -U NONE -i NONE -V1 -e -s | :
+
 

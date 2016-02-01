7b0fdd6a51c2615725fa4df89bc3c9ff0adb7ce1
assout <assout@users.noreply.github.com>
2016-02-01 21:41:34 +0900

    Fix install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-REM TODO 既存リンク上書き
+﻿REM TODO 既存リンク上書き
 
 mklink /J "%HOME%\dotfiles" "%HOME%\Development\dotfiles"
 
@@ -11,13 +11,19 @@ mklink "%HOME%\.gitignore" "%HOME%\Development\dotfiles\.gitignore"
 mklink "%HOME%\.inputrc" "%HOME%\Development\dotfiles\.inputrc"
 mklink "%HOME%\.minttyrc" "%HOME%\Development\dotfiles\.minttyrc"
 mklink "%HOME%\.tmux.conf" "%HOME%\Development\dotfiles\.tmux.conf"
+
 mklink "%HOME%\_gvimrc" "%HOME%\Development\dotfiles\vim\.gvimrc"
 mklink "%HOME%\_vimrc" "%HOME%\Development\dotfiles\vim\.vimrc"
+
 REM TODO Eclipse Marsは%USERPROFILE%を見る。Keplerは%HOME%を見る。
+REM TODO Eclipse Marsは.vrapperrcを見る。Keplerは_vrapperrcを見る。
 mklink "%HOME%\.vrapperrc.environment" "%HOME%\Development\dotfiles\eclipse\_vrapperrc.win"
 mklink "%USERPROFILE%\.vrapperrc.environment" "%HOME%\Development\dotfiles\eclipse\_vrapperrc.win"
 mklink "%HOME%\_vrapperrc" "%HOME%\Development\dotfiles\eclipse\_vrapperrc"
-mklink "%USERPROFILE%\_vrapperrc" "%HOME%\Development\dotfiles\eclipse\_vrapperrc"
+mklink "%USERPROFILE%\.vrapperrc" "%HOME%\Development\dotfiles\eclipse\_vrapperrc"
+
+mklink "%HOME%\.mdlrc" "%HOME%\Development\dotfiles\lint\.mdlrc"
+mklink "%HOME%\.mdlrc.style.rb" "%HOME%\Development\dotfiles\lint\.mdlrc.style.rb"
 
 git config user.name "assout"
 git config user.email "assout@users.noreply.github.com"

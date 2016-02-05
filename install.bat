27a8c8ecc7c680c4a770085999d01f482203c2d3
assout <assout@users.noreply.github.com>
2016-02-05 19:15:57 +0900

    Fix format

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
-REM TODO 既存リンク上書き
-REM TODO 改行コードをdosにする
-REM TODO %HOME%未定義のとき考慮
+REM TODO: 既存リンク上書き
+REM TODO: 改行コードをdosにする
+REM TODO: %HOME%未定義のとき考慮
 
 mklink /J "%HOME%\dotfiles" "%HOME%\Development\dotfiles"
 
@@ -17,8 +17,8 @@ mklink "%HOME%\.tmux.conf" "%HOME%\Development\dotfiles\.tmux.conf"
 mklink "%HOME%\_gvimrc" "%HOME%\Development\dotfiles\vim\.gvimrc"
 mklink "%HOME%\_vimrc" "%HOME%\Development\dotfiles\vim\.vimrc"
 
-REM TODO Eclipse Marsは%USERPROFILE%を見る。Keplerは%HOME%を見る。
-REM TODO Eclipse Marsは.vrapperrcを見る。Keplerは_vrapperrcを見る。
+REM TODO: Eclipse Marsは%USERPROFILE%を見る。Keplerは%HOME%を見る。
+REM TODO: Eclipse Marsは.vrapperrcを見る。Keplerは_vrapperrcを見る。
 mklink "%HOME%\.vrapperrc.environment" "%HOME%\Development\dotfiles\eclipse\_vrapperrc.win"
 mklink "%USERPROFILE%\.vrapperrc.environment" "%HOME%\Development\dotfiles\eclipse\_vrapperrc.win"
 mklink "%HOME%\_vrapperrc" "%HOME%\Development\dotfiles\eclipse\_vrapperrc"

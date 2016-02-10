a6d8e88e460f6f0396e3c59134466656b24218e3
assout <assout@users.noreply.github.com>
2016-02-10 21:22:22 +0900

    Fix install.bat, tmux.conf

--- install.bat
+++ install.bat
@@ -1,32 +1,34 @@
-REM TODO: 既存リンク上書き
-REM TODO: 改行コードをdosにする
-REM TODO: %HOME%未定義のとき考慮
+rem TODO: 既存リンク上書き
+rem TODO: %HOME%未定義のとき考慮
 
-mklink /J "%HOME%\dotfiles" "%HOME%\Development\dotfiles"
+rem 当ファイル自身のパス
+set here=%~dp0
 
-mklink "%HOME%\Documents\AutoHotkey.ahk" "%HOME%\Development\dotfiles\AutoHotkey.ahk"
+mklink /J "%HOME%\dotfiles" "%here%"
+mklink "%HOME%\Documents\AutoHotkey.ahk" "%here%\AutoHotkey.ahk"
 
-mklink "%HOME%\.bashrc" "%HOME%\Development\dotfiles\.bashrc"
-mklink "%HOME%\.ctags" "%HOME%\Development\dotfiles\.ctags"
-mklink "%HOME%\.gitconfig" "%HOME%\Development\dotfiles\.gitconfig"
-mklink "%HOME%\.gitignore" "%HOME%\Development\dotfiles\.gitignore"
-mklink "%HOME%\.gitattributes" "%HOME%\Development\dotfiles\.gitattributes"
-mklink "%HOME%\.inputrc" "%HOME%\Development\dotfiles\.inputrc"
-mklink "%HOME%\.minttyrc" "%HOME%\Development\dotfiles\.minttyrc"
-mklink "%HOME%\.tmux.conf" "%HOME%\Development\dotfiles\.tmux.conf"
+mklink "%HOME%\.bashrc" "%here%\.bashrc"
+mklink "%HOME%\.ctags" "%here%\.ctags"
+mklink "%HOME%\.gitconfig" "%here%\.gitconfig"
+mklink "%HOME%\.gitignore" "%here%\.gitignore"
+mklink "%HOME%\.gitattributes" "%here%\.gitattributes"
+mklink "%HOME%\.inputrc" "%here%\.inputrc"
+mklink "%HOME%\.minttyrc" "%here%\.minttyrc"
+mklink "%HOME%\.tmux.conf" "%here%\.tmux.conf"
 
-mklink "%HOME%\_gvimrc" "%HOME%\Development\dotfiles\vim\.gvimrc"
-mklink "%HOME%\_vimrc" "%HOME%\Development\dotfiles\vim\.vimrc"
+mklink "%HOME%\_gvimrc" "%here%\vim\.gvimrc"
+mklink "%HOME%\_vimrc" "%here%\vim\.vimrc"
 
-REM TODO: Eclipse Marsは%USERPROFILE%を見る。Keplerは%HOME%を見る。
-REM TODO: Eclipse Marsは.vrapperrcを見る。Keplerは_vrapperrcを見る。
-mklink "%HOME%\.vrapperrc.environment" "%HOME%\Development\dotfiles\eclipse\_vrapperrc.win"
-mklink "%USERPROFILE%\.vrapperrc.environment" "%HOME%\Development\dotfiles\eclipse\_vrapperrc.win"
-mklink "%HOME%\_vrapperrc" "%HOME%\Development\dotfiles\eclipse\_vrapperrc"
-mklink "%USERPROFILE%\.vrapperrc" "%HOME%\Development\dotfiles\eclipse\_vrapperrc"
+rem TODO: Eclipse Marsは%USERPROFILE%を見る。Keplerは%HOME%を見る。
+rem TODO: Eclipse Marsは.vrapperrcを見る。Keplerは_vrapperrcを見る。
+mklink "%HOME%\.vrapperrc.environment" "%here%\eclipse\_vrapperrc.win"
+mklink "%USERPROFILE%\.vrapperrc.environment" "%here%\eclipse\_vrapperrc.win"
+mklink "%HOME%\_vrapperrc" "%here%\eclipse\_vrapperrc"
+mklink "%USERPROFILE%\.vrapperrc" "%here%\eclipse\_vrapperrc"
 
-mklink "%HOME%\.mdlrc" "%HOME%\Development\dotfiles\lint\.mdlrc"
-mklink "%HOME%\.mdlrc.style.rb" "%HOME%\Development\dotfiles\lint\.mdlrc.style.rb"
+mklink "%HOME%\.mdlrc" "%here%\lint\.mdlrc"
+mklink "%HOME%\.mdlrc.style.rb" "%here%\lint\.mdlrc.style.rb"
 
 git config user.name "assout"
 git config user.email "assout@users.noreply.github.com"
+

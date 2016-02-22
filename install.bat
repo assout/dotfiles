e2fe9e694c07098a3dad7a67ad2b68cc574bba2f
assout <assout@users.noreply.github.com>
2016-02-23 04:10:20 +0900

    Fix comment

--- install.bat
+++ install.bat
@@ -19,10 +19,10 @@ mklink "%HOME%\.tmux.conf" "%here%\.tmux.conf"
 mklink "%HOME%\_gvimrc" "%here%\vim\.gvimrc"
 mklink "%HOME%\_vimrc" "%here%\vim\.vimrc"
 
-rem TODO: Eclipse Marsは%USERPROFILE%の".vrapperrc"を見る。
+rem Note: Eclipse Marsは%USERPROFILE%の".vrapperrc"を見る。
 mklink "%USERPROFILE%\.vrapperrc" "%here%\eclipse\_vrapperrc"
 mklink "%USERPROFILE%\.vrapperrc.environment" "%here%\eclipse\_vrapperrc.win"
-rem TODO: Eclipse Keplerは%HOME%の"_vrapperrc"を見る。
+rem Note: Eclipse Keplerは%HOME%の"_vrapperrc"を見る。
 mklink "%HOME%\_vrapperrc" "%here%\eclipse\_vrapperrc"
 mklink "%HOME%\.vrapperrc.environment" "%here%\eclipse\_vrapperrc.win"
 

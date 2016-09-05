bab31fc4c21c884156a480b61349aa9de277142a
assout <assout@users.noreply.github.com>
2016-09-05 19:10:41 +0900

    [WIP]  symlink.sh | 56 ++++++++++++++++++++++++++++----------------------------  1 file changed, 28 insertions(+), 28 deletions(-)

--- symlink.sh
+++ symlink.sh
@@ -10,43 +10,43 @@ is_office=$(if [ "${USERNAME}" = admin ] ; then echo 0 ; fi)
 
 if [ "${is_win}" ] ; then
   # Note: Eclipse Marsは%USERPROFILE%の".vrapperrc"を見る。
-  ln -sfb "${HERE}/eclipse/.vrapperrc" "$(cygpath "${USERPROFILE}")/.vrapperrc"
-  ln -sfb "${HERE}/eclipse/.vrapperrc.win" "$(cygpath "${USERPROFILE}")/.vrapperrc.env"
-  ln -sfb "${HERE}/eclipse/eclipse.ini.win" ~/Tools/eclipse-java-mars-2-win32-x86_64/eclipse/eclipse.ini
+  ln -sf "${HERE}/eclipse/.vrapperrc" "$(cygpath "${USERPROFILE}")/.vrapperrc"
+  ln -sf "${HERE}/eclipse/.vrapperrc.win" "$(cygpath "${USERPROFILE}")/.vrapperrc.env"
+  ln -sb "${HERE}/eclipse/eclipse.ini.win" ~/Tools/eclipse-java-mars-2-win32-x86_64/eclipse/eclipse.ini
 else
-  ln -sfb "${HERE}/eclipse/.vrapperrc" ~/.vrapperrc
-  ln -sfb "${HERE}/eclipse/.vrapperrc.linux" ~/.vrapperrc.env
+  ln -sf "${HERE}/eclipse/.vrapperrc" ~/.vrapperrc
+  ln -sf "${HERE}/eclipse/.vrapperrc.linux" ~/.vrapperrc.env
   # TODO eclipse.ini.linux
 fi
 
-ln -sfb "${HERE}/lint/.mdlrc" ~/
-ln -sfb "${HERE}/lint/.mdlrc.style.rb" ~/
-ln -sfb "${HERE}/lint/.eslintrc.yml" ~/
-ln -sfb "${HERE}/lint/.textlintrc" ~/
-ln -sfb "${HERE}/lint/.textlintrc" ~/
-ln -sfb "${HERE}/lint/.prh.yml" ~/
-ln -sfb "${HERE}/vim/.gvimrc" ~/
-ln -sfb "${HERE}/vim/.vimrc" ~/
-ln -sfb "${HERE}/.bashrc" ~/
-ln -sfb "${HERE}/.ctags" ~/
-ln -sfb "${HERE}/.gitconfig" ~/
+ln -sf "${HERE}/lint/.mdlrc" ~/
+ln -sf "${HERE}/lint/.mdlrc.style.rb" ~/
+ln -sf "${HERE}/lint/.eslintrc.yml" ~/
+ln -sf "${HERE}/lint/.textlintrc" ~/
+ln -sf "${HERE}/lint/.textlintrc" ~/
+ln -sf "${HERE}/lint/.prh.yml" ~/
+ln -sf "${HERE}/vim/.gvimrc" ~/
+ln -sf "${HERE}/vim/.vimrc" ~/
+ln -sf "${HERE}/.bashrc" ~/
+ln -sf "${HERE}/.ctags" ~/
+ln -sf "${HERE}/.gitconfig" ~/
 if [ "${is_home}" ] && [ "${is_unix}" ] ; then
-  ln -sfb "${HERE}/.gitconfig.home.linux" ~/.gitconfig.env
+  ln -sf "${HERE}/.gitconfig.home.linux" ~/.gitconfig.env
 elif [ "${is_home}" ] && [ "${is_win}" ] ; then
-  ln -sfb "${HERE}/.gitconfig.home.win" ~/.gitconfig.env
+  ln -sf "${HERE}/.gitconfig.home.win" ~/.gitconfig.env
 elif [ "${is_office}" ] ; then
-  ln -sfb "${HERE}/.gitconfig.office" ~/.gitconfig.env
+  ln -sf "${HERE}/.gitconfig.office" ~/.gitconfig.env
 fi
-ln -sfb "${HERE}/.gitignore" ~/
-ln -sfb "${HERE}/.gitattributes" ~/
-ln -sfb "${HERE}/.git_templates" ~/
-ln -sfb "${HERE}/.inputrc" ~/
-ln -sfb "${HERE}/.tmux.conf" ~/
-ln -sfb "${HERE}/.todo" ~/
-ln -sfb "${HERE}/.tern-project" ~/
+ln -sf "${HERE}/.gitignore" ~/
+ln -sf "${HERE}/.gitattributes" ~/
+ln -sf "${HERE}/.git_templates" ~/
+ln -sf "${HERE}/.inputrc" ~/
+ln -sf "${HERE}/.tmux.conf" ~/
+ln -sf "${HERE}/.todo" ~/
+ln -sf "${HERE}/.tern-project" ~/
 
 if [ "${is_win}" ] ; then
-  ln -sfb "${HERE}/AutoHotKey.ahk" ~/Documents/
-  ln -sfb "${HERE}/.minttyrc" ~/
+  ln -sf "${HERE}/AutoHotKey.ahk" ~/Documents/
+  ln -sf "${HERE}/.minttyrc" ~/
 fi
 

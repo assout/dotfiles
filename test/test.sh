857aa4ae35aaad803c6f8a85b3b881ba55739fa6
assout <assout@users.noreply.github.com>
2016-11-01 22:37:44 +0900

    WIP:  .travis.yml  | 19 ++++++++++---------  test/test.sh | 12 ++++++++++++  2 files changed, 22 insertions(+), 9 deletions(-)

---
+++ test/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+vim --cmd version --cmd quit
+
+vint --version
+vint vim/.vimrc
+vint vim/.gvimrc
+# TODO: vlmlparser通らないっぽいからダメ - vint eclipse/_vrapperrc
+
+bashate -i E002,E003 .bashrc $(find -name "*.sh")
+shellcheck .bashrc $(find -name "*.sh")
+

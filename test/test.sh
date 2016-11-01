80f3b3186a0af7211a105dc7051015f4ac1dd8e6
assout <assout@users.noreply.github.com>
2016-11-01 22:55:57 +0900

    WIP:  .bashrc      | 4 ++--  test/test.sh | 4 ++--  vim/.vimrc   | 2 +-  3 files changed, 5 insertions(+), 5 deletions(-)

--- test/test.sh
+++ test/test.sh
@@ -7,6 +7,6 @@ vint vim/.vimrc
 vint vim/.gvimrc
 # TODO: vlmlparser通らないっぽいからダメ - vint eclipse/_vrapperrc
 
-bashate -i E002,E003 .bashrc $(find -name "*.sh")
-shellcheck .bashrc $(find -name "*.sh")
+bashate -i E002,E003 .bashrc "$(find -name "*.sh")"
+shellcheck .bashrc "$(find -name "*.sh")"
 

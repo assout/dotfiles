8d1c2e821ecea7c95beb5bb149d8fb67a6af2c74
assout <assout@users.noreply.github.com>
2021-01-23 11:42:23 +0900

    WIP:  .bashrc.user | 160 ++++++++++++++++++++++++-----------------------------------  symlink.bat  |   2 +  2 files changed, 66 insertions(+), 96 deletions(-)

--- symlink.bat
+++ symlink.bat
@@ -1,5 +1,7 @@
 MKLINK C:\Users\porin\_gvimrc \\wsl$\Ubuntu\home\assout\.ghq\github.com\assout\dotfiles\vim\.gvimrc
 MKLINK C:\Users\porin\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json \\wsl$\Ubuntu\home\assout\.ghq\github.com\assout\dotfiles\WindowsTerminal\settings.json
+rem WSL起動の前にスタートアップが実行されると起動されない？
 MKLINK "C:\Users\porin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AutoHotkey.ahk" \\wsl$\Ubuntu\home\assout\.ghq\github.com\assout\dotfiles\AutoHotkey.ahk
+cp AutoHotkey.ahk "C:\Users\porin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AutoHotkey.ahk" 
 pause
 
